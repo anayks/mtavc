1dff1e322909348b45fbe684752aff75c0c60772
Kirill <anayks@yandex.ru>
2021-02-04 20:55:44 +0300

    Configs
    All changes are about split configs from main

---
+++ configs/server/factions/names.lua
@@ -0,0 +1,16 @@
+local FactionNames = 
+{
+	[1] = {[1]="La Cosa Nostra", [2]="La Cosa Nostra"},
+	[2] = {[1]="Colombian Mafia", [2]="Colombian Mafia"},
+	[3] = {[1]="Mexican cartel", [2]="Mexican cartel"},
+	[4] = {[1]="Triada", [2]="Triada"},
+	[5] = {[1]="City Hall of Vice", [2]="[City Hall]"},
+	[6] = {[1]="Vice News", [2]="[VCN]"},
+	[7] = {[1]="Vice City Hospital", [2]="[EMS]"},
+	[8] = {[1]="Vice City Police Department", [2]="[Police]"},
+	[9]	= {[1]="Vice City Army", [2]="[Army]"}
+}
+
+function getFactionsNames()
+  return FactionNames
+end
